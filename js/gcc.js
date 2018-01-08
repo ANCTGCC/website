9d6c60c748716f9d5afc9da5c1d391e5221eb5dc
kuron334 <e1542@s.akashi.ac.jp>
2018-01-08 18:40:18 +0900

    htmlの構造を変更

--- js/gcc.js
+++ js/gcc.js
@@ -1,3 +1,13 @@
+//サブコンテンツmain-subcontents
+$(function(){
+  $("#main-subcontents").load("main-subcontents.html");
+});
+//footer.htmlを読み込み
+$(function(){
+  $("#footer").load("footer.html");
+});
+
+
 //ハンバーガーメニューを押すとメニューがスライド
 $(function(){
   $("#jQueryPush").click(function(){
@@ -5,58 +15,7 @@ $(function(){
   });
 });
 
-//TOPへ戻るのボタン
-// グローバル変数
-var syncerTimeout = null;
-// 一連の処理
-$(function(){
-  // スクロールイベントの設定
-  $(window).scroll(function(){
-    // 1秒ごとに処理
-    if(syncerTimeout == null){
-      // セットタイムアウトを設定
-      syncerTimeout = setTimeout(function(){
-        // 対象のエレメント
-        var element = $('#page-top');
-        // 現在、表示されているか？
-        var visible = element.is(':visible');
-        // 最上部から現在位置までの距離を取得して、変数[now]に格納
-        var now = $(window).scrollTop();
-        // 最下部から現在位置までの距離を計算して、変数[under]に格納
-        var under = $('body').height() - (now + $(window).height());
-        // 最上部から現在位置までの距離(now)が1300以上かつ
-        // 最下部から現在位置までの距離(under)が0px以上かつ…
-        if(now >= 1300 && 0 <= under){
-          // 非表示状態だったら
-          if(!visible){
-            // [#page-top]をゆっくりフェードインする
-            element.fadeIn('slow');
-          }
-        }
-        // 800px以下かつ表示状態だったら
-        else if(visible){
-          // [#page-top]をゆっくりフェードアウトする
-          element.fadeOut('slow') ;
-        }
-        // フラグを削除
-        syncerTimeout = null;
-      },1000);
-    }
-  });
-  // クリックイベントを設定する
-  $('#move-page-top').click(function(){
-    // スムーズにスクロールする
-    $('html,body').animate({scrollTop:0},'slow');
-  });
-});
 
-//折り畳みメニューの式
-$(function(){
-  $("#folder-menu").click(function(){
-    $("#folder-menu li").slideToggle("10");
-    $(".fa-chevron-circle-down").toggleClass("kaiten");
-  });
-});
 
 //メニューのアニメーション
 $(function(){
@@ -78,15 +37,4 @@ $(function(){
       opacity: 1,
     },1000);  
   });
-});
-
-//767px以下属性変更twitter欄
-$(window).on("load resize",function(){
-  var screen_width = $(window).width();
-  if (screen_width <= 767){
-    $(".timeline a").attr("data-height","200");
-  }
-  if (screen_width >= 768){
-    $(".timeline a").attr("data-height","400");
-  }
 });
